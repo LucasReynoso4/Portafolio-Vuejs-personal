20d5e604fa565048d4313ac7695104234b0ef6d9
Lucas <lucassreynoso4@gmail.com>
2024-11-10 11:43:46 -0300

    trabajo listo

---
+++ src/components/DatosPersonales.vue
@@ -0,0 +1,83 @@
+<script setup>
+
+import instagram from '/src/assets/instagram_icon.svg';
+import github from '/src/assets/github_icon.svg';
+
+
+const title = 'Lucas Reynoso ' ;
+const descripcion = 'Estudiante de la UTN Tecnicatura Universitaria en Programación ';
+const residencia = 'Longchamps, Buenos Aires, Argentina';
+const presentacion = 'Hola, Bienvenido a mi portafolio de proyectos. Soy un estudiante de la Tecnicatura en Programación ¡Gracias por la visita!.';
+const redesSociales = [
+
+  { id: 1, name: 'Instagram', src: instagram, url: 'https://www.instagram.com/reynosolucas_/' },
+  { id: 2, name: 'github', src: github, url: 'https://github.com/LucasReynoso4' },
+  
+];
+const telefono = '+54 11 6307 9508';
+</script>
+
+<template>
+  <section class="datos-personales">
+    <div class="card">
+      <h1>{{ title }}</h1>
+      <h2>{{ descripcion }}</h2>
+      <p>{{ presentacion }}</p>
+      <ul class="container-lista">
+        <li v-for="red in redesSociales" :key="red.id">
+          <a :href="red.url"><img class="icon-redsocial" :src="red.src" width="35rem" :alt="red.name"></a>
+        </li>
+      </ul>
+      <h3>☎ Mi Teléfono personal: {{ telefono }}</h3>
+      <h4>{{ residencia }}</h4>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+h1 {
+  font-size: 3.5rem;
+}
+
+p {
+  font-size: 1.2rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+}
+
+.card {
+  background-color: rgb(10, 111, 194);
+  border-radius: 10px;
+  padding: 30px;
+  margin: 30px;
+  text-align: center;
+}
+
+.container-lista {
+  display: flex;
+  justify-content: center;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  text-align: center;
+}
+
+.icon-redsocial {
+  align-items: center;
+  background-color: rgb(19, 119, 206);
+  border-radius: 50%;
+  padding: 2px;
+  margin: 5px;
+  box-shadow: 0 0 5px rgba(95, 124, 205, 0.934);
+}
+
+.icon-redsocial:hover {
+  background-color: rgb(28, 41, 52);
+  box-shadow: 0 0 5px rgba(251, 249, 249, 0.934);
+}
+
+h3 {
+  font-size: 1.5rem;
+  font-weight: 600;
+}
+</style>
---
+++ src/components/ExperienciaComponente.vue
@@ -0,0 +1,76 @@
+<script setup>
+import icono2 from '/src/assets/ecomerce.svg';
+import icono4 from '/src/assets/uiux.svg';
+import icono5 from '/src/assets/watch.svg';
+
+import { ref } from 'vue';
+const titulo = 'Desarrollador Jr';
+const fecha = 'Desde Enero 2021 / Noviembre 2024';
+const experiencias = ref([
+    { id: 1, src: icono2, parrafo: 'Diseñé y desarrollé aplicaciones web y juegos utilizando Python y React .' },
+    { id: 2, src: icono4, parrafo: 'Empleado en BNA Gestion de Activos.' },
+    { id: 3, src: icono5, parrafo: 'Termine la escuela secundaria en el Instituto Sendas Verdes .' },
+]);
+</script>
+
+<template>
+    <div class="card">
+        <h3 class="titulo">{{ titulo.toLocaleUpperCase() }}</h3>
+        <p class="fecha">{{ fecha }}</p>
+        <ul class="listado">
+            <li class="item" v-for="experencia in experiencias" :key="experencia.id">
+                <img class="imagen-svg" :src="experencia.src" width="45rem" :alt="experencia.parrafo">
+                <p>{{ experencia.parrafo }}</p>
+            </li>
+
+            <!-- <li class="item"><img class="imagen-svg" src="/src/assets/ventas.svg" width="45rem" alt="">
+                <p>Lideré un equipo de 5 desarrolladores en la creación de una plataforma de comercio electrónico que incrementó las ventas en un 35% durante el primer año.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/ecomerce.svg" width="45rem" alt="">
+                <p>Diseñé y desarrollé aplicaciones web utilizando Node.js, React y MongoDB, logrando reducir el tiempo de carga de las páginas en un 50%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/configuracion.svg" width="45rem" alt="">
+                <p>Implementé soluciones de autenticación y autorización, mejorando la seguridad del sitio web y reduciendo los intentos de acceso no autorizado en un 20%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/uiux.svg" width="45rem" alt="">
+                <p>Colaboré con diseñadores de UX/UI para crear interfaces de usuario atractivas y fáciles de usar, aumentando la satisfacción del cliente en un 15%.</p>
+            </li>
+            <li class="item"><img class="imagen-svg" src="/src/assets/watch.svg" width="45rem" alt="">
+                <p>Automaticé procesos de despliegue continuo utilizando Jenkins y Docker, reduciendo los tiempos de despliegue en un 40%.</p>
+            </li> -->
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    display: flex;
+    flex-direction: column;
+    padding: 2rem;
+    background-color: rgb(28, 41, 52);
+    border-radius: 15px;
+}
+
+.titulo {
+    font-size: 1.5rem;
+    color: coral;
+}
+
+.fecha {
+    font-size: 1rem;
+    color: burlywood;
+    margin-bottom: 1rem;
+}
+
+.listado {
+    display: flex;
+    flex-direction: column;
+}
+
+.item {
+    align-items: center;
+    display: flex;
+    padding: 1rem;
+    gap: 1.5rem;
+}
+</style>
